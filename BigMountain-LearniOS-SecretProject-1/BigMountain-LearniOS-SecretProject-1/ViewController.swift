7f2e55bcd59589b8f9272ba5b5630cd38df9925f
Jonathon Juvenal <jonathon.juvenal@gmail.com>
2018-10-16 16:06:18 -0600

    Initial Project Creation

---
+++ BigMountain-LearniOS-SecretProject-1/BigMountain-LearniOS-SecretProject-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BigMountain-LearniOS-SecretProject-1
+//
+//  Created by Jonathon Juvenal on 10/16/18.
+//  Copyright © 2018 Jonathon Juvenal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
